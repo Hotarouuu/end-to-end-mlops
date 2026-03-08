7b7c9b3c994d3babad446d08972a1a81d88180a5
Hotarouuu <lucashondo01@proton.me>
2026-03-08 16:21:21 -0400

    Refactoring how the model is created [skip ci]
    Changes:
    
    - Preprocessor class was discontinued. Now we are doing all the preprocessing job in the model class. Thanks to this change the training script is simpler
    - MLFlow autolog now registers the model every time we train a new model
    - We are refactoring the way our containers work. The plan is, at least for now, to keep the data locally inside of the MLFlow container.
    
    Future plans/suggestions:
    
    - Have full integration with the AWS services. So pushing the images to ECR, building with EC2, log the artifacts in a S3 bucket and store our data in a database.

--- src/farm_detection/models/model.py
+++ src/farm_detection/models/model.py
@@ -1,5 +1,30 @@
 from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+import numpy as np
 
 
-def GNB(priors=None, var_smoothing=1e-9):
-    return GaussianNB(priors=priors, var_smoothing=var_smoothing)
+class GNBWithEncoding:
+    def __init__(self, priors=None, var_smoothing=1e-9):
+        self.label_encoder = LabelEncoder()
+        self.pipeline = Pipeline([
+            ('scaler', StandardScaler()),
+            ('gnb', GaussianNB(priors=priors, var_smoothing=var_smoothing))
+        ])
+
+    def log_transform(self, X):
+        X["humidity_log"] = np.log(X["humidity"] + 1)
+        X["rainfall_log"] = np.log(X["rainfall"] + 1)
+        X.drop(["humidity", "rainfall"], axis=1, inplace=True)
+        return X
+    
+    def fit(self, X, y):
+        X = self.log_transform(X)
+        y_encoded = self.label_encoder.fit_transform(y)
+        self.pipeline.fit(X, y_encoded)
+        return self
+    
+    def predict(self, X):
+        X = self.log_transform(X)
+        y_pred = self.pipeline.predict(X)
+        return self.label_encoder.inverse_transform(y_pred)
